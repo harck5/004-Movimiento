41b6cd2fe13665abe52783cc15b4060ae9d400b5
harck5 <raaaaullopez@gmail.com>
2022-11-15 12:56:05 +0100

    auto bus completo

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 20f;
-    public float turnSpeed = 10f;
-    public float horizontalInput;
+    private float speed = 20f;
+    private float turnSpeed = 40f;
+    private float horizontalInput;
+    private float verticalInput;
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(0, 0, 1 * speed * Time.deltaTime);
-        transform.Translate(Vector3.right * turnSpeed * Time.deltaTime * horizontalInput);
+        verticalInput = Input.GetAxis("Vertical");
+        //transform.Translate(0, 0, 1 * speed * Time.deltaTime);
+        //transform.Translate(Vector3.right * turnSpeed * Time.deltaTime * horizontalInput);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime * verticalInput);
+        //transform.Rotate(Vector3.up * turnSpeed * Time.deltaTime * horizontalInput);
+        if (verticalInput > 0) 
+        {
+            transform.Rotate(Vector3.up * turnSpeed * Time.deltaTime * horizontalInput);
+        }
+        if (verticalInput < 0)
+            {
+            transform.Rotate(-Vector3.up * turnSpeed * Time.deltaTime * horizontalInput);
+            }
     }
 }
